7ba84280e46c428770b1fd717eccb46bb2766853
Marco <marco01spinelli@gmail.com>
2022-11-03 08:53:25 +0100

    commit secondo

---
+++ app/src/main/java/com/example/mfl/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mfl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
