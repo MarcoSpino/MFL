ec5136ebdd93ad4c3fe5b1636da9453c1a1e31c7
Marco <marco01spinelli@gmail.com>
2022-11-03 11:17:18 +0100

    commit quartosss

--- app/src/main/java/com/example/mfl/MainActivity.java
+++ app/src/main/java/com/example/mfl/MainActivity.java
@@ -7,13 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
 
     public void reg(View View){
