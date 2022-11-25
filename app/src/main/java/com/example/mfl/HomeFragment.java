45e3935352dbf2668c8486d5e8bf6feded55fce8
Frappeallafrutta <64410335+Frappeallafrutta@users.noreply.github.com>
2022-11-25 23:47:43 +0100

    modifiche pagina iniziale, ho messo a posto il mantenimento del login e ho iniziato la pagina profilo

--- app/src/main/java/com/example/mfl/HomeFragment.java
+++ app/src/main/java/com/example/mfl/HomeFragment.java
@@ -1,42 +1,22 @@
 package com.example.mfl;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import android.os.Handler;
-import android.util.Log;
+
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ViewFlipper;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.android.volley.toolbox.StringRequest;
 
 
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
+
+
 
 
 
@@ -51,10 +31,6 @@ public class HomeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     String url = "http://www.giantbomb.com/api/game/3030-4725/?api_key=6cda058679cecae3b068ecd416ab3eab3e2c5baf";
 
@@ -88,8 +64,9 @@ public class HomeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            // TODO: Rename and change types of parameters
+            String mParam1 = getArguments().getString(ARG_PARAM1);
+            String mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
     @Override
@@ -100,26 +77,12 @@ public class HomeFragment extends Fragment {
 
 
         prova = (TextView) viewGroup.findViewById(R.id.prova1);
-        RequestQueue queue = Volley.newRequestQueue(this.getActivity());
+
 
 
 
 
         return viewGroup;
     }
-    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-            (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
-                @Override
-                public void onResponse(JSONObject response) {
-                    prova.setText("Response: " + response.toString());
-                }
-            }, new Response.ErrorListener() {
-
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    // TODO: Handle error
-
-                }
-            });
 }
--- app/src/main/java/com/example/mfl/home.java
+++ app/src/main/java/com/example/mfl/home.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
